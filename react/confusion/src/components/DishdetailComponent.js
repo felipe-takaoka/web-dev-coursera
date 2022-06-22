f31bdc5e640708a6a54ff540f22487ae5bdbfd12
Felipe Takaoka <felipe.takaoka@nubank.com.br>
2022-06-22 01:46:42 -0300

    feat: Week 1 assignment

---
+++ react/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,76 @@
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import React, { Component } from 'react';
+
+class Dishdetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  renderDish() {
+    let dish = this.props.dish;
+    
+    return (
+      <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    )
+  }
+
+  renderComments(comments) {
+    if (comments != null) {
+      return (
+        <div>
+          <h4>Comments</h4>
+          <ul className='list-unstyled'>
+            {comments.map(comment => {
+              let id = comment.id;
+              let commentText = comment.comment;
+              let author = comment.author;
+              let options = { year: 'numeric', month: 'short', day: 'numeric' };
+              let formattedDate = new Date(comment.date).toLocaleDateString("en-US", options);
+
+              return (
+                <div key={id}>
+                  <li key={id}>
+                    <blockquote className="blockquote">
+                      <p className="mb-0">{commentText}</p>
+                      <footer className="blockquote-footer">{author}, {formattedDate}</footer>
+                    </blockquote>
+                  </li>
+                </div>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    } else {
+      return (<div></div>)
+    }
+  }
+
+  render() {
+    if (this.props.dish != null) {
+      return (
+        <div className='row'>
+          <div className='col-12 col-md-5 m-1'>
+            {this.renderDish()}
+          </div>
+
+          <div className='col-12 col-md-5 m-1'>
+            {this.renderComments(this.props.dish.comments)}            
+          </div>
+        </div>
+      );
+    } else {
+      return (<div></div>);
+    }
+  }
+}
+
+export default Dishdetail;
