7b862ad654097e1dfd2da2498db992697bb1f4e3
Felipe Takaoka <fehtakaoka@gmail.com>
2022-07-23 00:10:59 -0300

    feat: Post feedback to json-server

--- react/confusion/src/components/AboutComponent.js
+++ react/confusion/src/components/AboutComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
             <RenderLeader leader={leader} />
         );
@@ -15,7 +17,7 @@ function About(props) {
             <div key={leader.id} className='col-12 mt-5'>
                 <Media tag='li'>
                     <Media left middle>
-                        <Media object src={leader.image} alt={leader.name} />
+                        <Media object src={baseUrl + leader.image} alt={leader.name} />
                     </Media>
                     <Media body className='ml-5'>
                         <Media heading>{leader.name}</Media>
@@ -81,11 +83,10 @@ function About(props) {
                 <div className="col-12">
                     <h2>Corporate Leadership</h2>
                 </div>
-                <div className="col-12">
-                    <Media list>
-                        {leaders}
-                    </Media>
-                </div>
+                <div className="col-12">{
+                    props.leaders.isLoading ? <Loading /> :
+                        (props.leaders.errMess ? <h4>{props.leaders.errMess}</h4> : <Media list>{leaders}</Media>)
+                }</div>
             </div>
         </div>
     );
