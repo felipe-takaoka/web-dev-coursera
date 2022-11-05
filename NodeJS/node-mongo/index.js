59883994dd42282a965367829f36fc3509fa3d87
Felipe Takaoka <fehtakaoka@gmail.com>
2022-11-05 17:09:39 -0300

    feat: Create API module for interacting with mongo

--- NodeJS/node-mongo/index.js
+++ NodeJS/node-mongo/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
+const dboper = require("./operations");
 
 const uri = process.env.MONGO_URI;
 const dbName = "conFusion";
@@ -11,25 +12,31 @@ MongoClient.connect(uri, (err, client) => {
   console.log("Connected to server");
 
   const db = client.db(dbName);
-  const collection = db.collection("dishes");
+  dboper.insertDocument(
+    db,
+    { name: "Vadonut", description: "Test" },
+    "dishes",
+    (result) => {
+      console.log(`Insert document\n${result.ops}`);
+      dboper.findDocuments(db, "dishes", (docs) => {
+        console.log(`Found documents\n${docs}`);
 
-  collection.insertOne(
-    { name: "Uthappizza2", description: "test2" },
-    (err, res) => {
-      assert.equal(err, null);
-      console.log("After insert:\n");
-      console.log(res.ops);
-
-      collection.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        console.log("Found:\n");
-        console.log(docs);
-
-        db.dropCollection("dishes", (err, res) => {
-          assert.equal(err, null);
-
-          client.close();
-        });
+        dboper.updateDocument(
+          db,
+          { name: "Vadonut" },
+          { description: "Update Test" },
+          "dishes",
+          (docs) => {
+            console.log(`Updated Document\n${result.result}`);
+            dboper.findDocuments(db, "dishes", (docs) => {
+              console.log(`Found documents\n${docs}`);
+              db.dropCollection("dishes", (result) => {
+                console.log(`Drop collection ${result}`);
+                client.close();
+              });
+            });
+          }
+        );
       });
     }
   );
