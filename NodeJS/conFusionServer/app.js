9efc10b65fd99311239543b2eb26184282b49d72
Felipe Takaoka <fehtakaoka@gmail.com>
2022-11-11 09:18:28 -0300

    feat: Add basic auth

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -34,6 +34,35 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    const err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+
+  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+
+  if (username === "admin" && password == "password") {
+    next();
+  } else {
+    const err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
