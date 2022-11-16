0626b5a2e953ea7bec83f4cf513c4381e5638c4f
Felipe Takaoka <fehtakaoka@gmail.com>
2022-11-16 18:23:11 -0300

    feat: Implement file upload

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -9,6 +9,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var leaderRouter = require("./routes/leaderRouter");
+var uploadRouter = require("./routes/uploadRouter");
 var promoRouter = require("./routes/promoRouter");
 
 const mongoose = require("mongoose");
@@ -52,6 +53,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
